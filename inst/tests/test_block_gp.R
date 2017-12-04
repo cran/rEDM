033ff6a2b0b1c5e2dfa50e2f240eebe0372173c1
Hao Ye <hao.ye@weecology.org>
2017-12-04 22:21:36 +0000

    version 0.6.9

--- inst/tests/test_block_gp.R
+++ inst/tests/test_block_gp.R
@@ -2,7 +2,7 @@ library(rEDM)
 
 data("two_species_model")
 
-block <- two_species_model[1:200,]
+block <- two_species_model[1:200, ]
 
 # try different param combinations (no fitting)
 output <- block_gp(block, columns = c("x", "y"), 
@@ -10,12 +10,13 @@ output <- block_gp(block, columns = c("x", "y"),
                    v_e = seq(from = -300, to = 300, by = 50), 
                    eta = 7, 
                    fit_params = FALSE, 
-                   first_column_time = TRUE, stats_only = FALSE)
+                   first_column_time = TRUE, stats_only = FALSE, 
+                   silent = TRUE)
 plot(output$model_output[[4]]$obs, 
      output$model_output[[4]]$pred)
 
 # multivariate simplex projection using x and y to predict x
 output <- block_gp(block, columns = c("x", "y"), 
                    first_column_time = TRUE, stats_only = FALSE, 
-                   save_covariance_matrix = TRUE)
-
+                   save_covariance_matrix = TRUE, 
+                   silent = TRUE)
