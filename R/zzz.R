6d4dd3e937638749e65cbb5a7fbd5e99ec285a65
George Sugihara <sugihara.lab.manager@gmail.com>
2020-03-06 08:20:05 +0000

    version 1.2.3

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,12 @@
-loadModule("rEDMInternal", TRUE)
+Rcpp::loadModule("lnlp_module", TRUE)
+Rcpp::loadModule("block_lnlp_module", TRUE)
+Rcpp::loadModule("xmap_module", TRUE)
+
+.onAttach <- function(...) {
+    if (!interactive()) return()
+    
+    intro_message <- paste("We are currently building a new version of rEDM, and a version for python pyEDM.",
+"Be on the look out for version 1.0 for rEDM.",
+"Updates can be found at https://github.com/SugiharaLab", sep = " ")
+    packageStartupMessage(intro_message)
+}
